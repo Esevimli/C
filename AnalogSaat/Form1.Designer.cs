4bfd8223ca66b120f0f5f810a8cf9feb346b8740
Esevimli <131862472+Esevimli@users.noreply.github.com>
2023-04-27 13:27:37 +0300

    commit

---
+++ AnalogSaat/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace AnalogSaat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape2 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.ovalShape2,
+            this.ovalShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(434, 461);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // ovalShape1
+            // 
+            this.ovalShape1.BorderWidth = 3;
+            this.ovalShape1.Location = new System.Drawing.Point(20, 20);
+            this.ovalShape1.Name = "ovalShape1";
+            this.ovalShape1.Size = new System.Drawing.Size(400, 400);
+            // 
+            // ovalShape2
+            // 
+            this.ovalShape2.BackColor = System.Drawing.Color.Black;
+            this.ovalShape2.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape2.Location = new System.Drawing.Point(210, 210);
+            this.ovalShape2.Name = "ovalShape2";
+            this.ovalShape2.Size = new System.Drawing.Size(20, 20);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 461);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Veritabanı2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Veritabanı3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection bag = new OleDbConnection("Provider=Microsoft.Jet.Oledb.4.0;Data Source=vt.mdb");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OleDbCommand komut = new OleDbCommand("SELECT * FROM KAYIT", bag);
+            OleDbDataAdapter adp = new OleDbDataAdapter(komut);
+            DataTable tablo = new DataTable();
+            adp.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string isim;
+            if (textBox1.Text != "")
+                isim = textBox1.Text;
+            else
+                isim = "%";
+
+            string sorgu = "SELECT * FROM KAYIT WHERE AD LIKE @ad  ";//@ ifadesi sorguya parametre eklemek için kullanılır
+            OleDbCommand komut = new OleDbCommand(sorgu, bag);
+            komut.Parameters.AddWithValue("@ad", isim);//sorgudaki ad parametresine isim değişkenindeki değeri verirsin
+            OleDbDataAdapter adp = new OleDbDataAdapter(komut);
+            DataTable tablo = new DataTable();
+            adp.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }
+    }
+}
